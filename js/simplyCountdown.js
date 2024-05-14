5ca1786e7ebb5b89f4d485706fe3b5453e544946
Alexander <31966644+Be10@users.noreply.github.com>
2024-05-14 10:14:46 -0500

    Update simplyCountdown.js

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,7 +115,7 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2025,
+                year: 2024,
                 month: 5,
                 day: 5,
                 hours: 9,
